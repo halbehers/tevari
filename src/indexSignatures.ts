d22877c1b9ebe6bef601f33feb5138bb85baceee
halbehers <sebastien.halbeher@free.fr>
2024-02-07 18:43:08 +0100

    Add documentation to index signature

--- src/indexSignatures.ts
+++ src/indexSignatures.ts
@@ -1,6 +1,13 @@
 import { arraysAreSame } from "./arrays";
 import { Function1 } from "./functions";
 
+/**
+ * Creates an index signature from the given array.
+ * 
+ * @param array The array to convert.
+ * @param idExtractor A function to extract the id from each array elements.
+ * @returns the created index signature.
+ */
 export const indexSignatureFromArray = <T>(
   array: T[],
   idExtractor: (element: T) => string
@@ -12,6 +19,14 @@ export const indexSignatureFromArray = <T>(
   return result;
 };
 
+/**
+ * Creates an index signature from the given array with every elements mapped using the given valueExtractor.
+ * 
+ * @param array The array to convert.
+ * @param idExtractor A function to extract the id from each array elements.
+ * @param valueExtractor A mapper function to convert each element of the array from T to U.
+ * @returns the created index signature.
+ */
 export const indexSignatureFromArrayValues = <T, U>(
   array: T[],
   idExtractor: (element: T) => string,
@@ -24,34 +39,57 @@ export const indexSignatureFromArrayValues = <T, U>(
   return result;
 };
 
+/**
+ * Apply the provided mapping function to all values of the given index signature, and extract all values into an array.
+ * 
+ * @param indexSignature The index signature to map.
+ * @param mapper The mapper function.
+ * @param filter (optional) A filter predicate used to filter out unwanted values.
+ * @returns the result array of mapped values.
+ */
 export const indexSignatureMap = <T, U>(
   indexSignature: { [id: string]: T },
-  converter: (key: string, element: T) => U,
+  mapper: (key: string, element: T) => U,
   filter?: (key: string, element: T) => boolean
 ): U[] => {
   const result: U[] = [];
   for (const [id, element] of Object.entries(indexSignature).filter(
     ([id, element]) => !filter || filter(id, element)
   )) {
-    result.push(converter(id, element));
+    result.push(mapper(id, element));
   }
   return result;
 };
 
+/**
+ * Apply the provided mapping function to all values of the given index signature, and extract all values into an array.
+ * 
+ * @param indexSignature The index signature to map.
+ * @param mapper The mapper function.
+ * @param filter (optional) A filter predicate used to filter out unwanted values.
+ * @returns the result array of mapped values.
+ */
 export const indexSignatureMapValues = <T, U>(
   indexSignature: { [id: string]: T },
-  converter: (element: T) => U,
+  mapper: (element: T) => U,
   filter?: (element: T) => boolean
 ): { [id: string]: U } => {
   const result: { [id: string]: U } = {};
   for (const [id, element] of Object.entries(indexSignature).filter(
     ([, element]) => !filter || filter(element)
   )) {
-    result[id] = converter(element);
+    result[id] = mapper(element);
   }
   return result;
 };
 
+/**
+ * Tests whether the two given index signatures are equals.
+ * 
+ * @param indexSignature1 The first index signature to compare.
+ * @param indexSignature2 The second index signature to compare.
+ * @returns `true` if the index signatures are equal, `false` otherwise.
+ */
 export const indexSignaturesAreSame = <T>(
   indexSignature1: { [id: string]: T },
   indexSignature2: { [id: string]: T }
@@ -70,6 +108,13 @@ export const indexSignaturesAreSame = <T>(
   return true;
 };
 
+/**
+ * Tests whether the given index signature is included into the given array of index signatures.
+ * 
+ * @param indexSignature The index signature to compare.
+ * @param indexSignature2 An array of index signatures to test against the first parameter.
+ * @returns `true` if the given index signatures is included into the array, `false` otherwise.
+ */
 export const indexSignatureIncludes = <T>(
   indexSignature: { [id: string]: T },
   indexSignatures: { [id: string]: T }[]
@@ -82,6 +127,16 @@ export const indexSignatureIncludes = <T>(
   return false;
 };
 
+/**
+ * Converts the given index signature into a string formated as follow:
+ * 
+ * ```
+ *    { key1: value1, key2: value2, key3: value3, ...}
+ * ```
+ * 
+ * @param indexSignature The index signature to convert.
+ * @returns a string representation of the given index signature.
+ */
 export const indexSignatureToString = <T>(indexSignature: {
   [id: string]: T;
 }): string => {
@@ -92,6 +147,12 @@ export const indexSignatureToString = <T>(indexSignature: {
   return `{ ${resultArray.join(", ")} }`;
 };
 
+/**
+ * Converts an index signature's values into an array.
+ * 
+ * @param indexSignature The index signature to convert.
+ * @returns an array of all values of the given index signature.
+ */
 export const indexSignatureToArray = <T>(indexSignature: {
   [id: string]: T;
 }): T[] => {
@@ -100,16 +161,29 @@ export const indexSignatureToArray = <T>(indexSignature: {
   return result;
 };
 
+/**
+ * Converts an index signature' keys and values converted by applying the given mapping function into an array.
+ * 
+ * @param indexSignature The index signature to convert.
+ * @param mapper The mapper function.
+ * @returns an array corresponding to all entries of the given index signature using the given mapper function.
+ */
 export const indexSignatureEntriesToArray = <T, U>(
   indexSignature: { [id: string]: T },
-  converter: (key: string, value: T) => U
+  mapper: (key: string, value: T) => U
 ): U[] => {
   const result: U[] = [];
   for (const [id, element] of Object.entries(indexSignature))
-    result.push(converter(id, element));
+    result.push(mapper(id, element));
   return result;
 };
 
+/**
+ * Converts an index signature's keys into an array.
+ * 
+ * @param indexSignature The index signature to convert.
+ * @returns an array of all keys of the given index signature.
+ */
 export const indexSignatureKeysToArray = <T>(indexSignature: {
   [id: string]: T;
 }): string[] => {
@@ -118,6 +192,13 @@ export const indexSignatureKeysToArray = <T>(indexSignature: {
   return result;
 };
 
+/**
+ * Filters out all entries from the given index signature that doesn't match the given predicate.
+ * 
+ * @param indexSignature The index signture to filter.
+ * @param filter Predicate to filter the entries.
+ * @returns an index signature containing only the element that match the given predicate.
+ */
 export const indexSignatureFilter = <T>(
   indexSignature: { [id: string]: T },
   filter: (id: string, element: T) => boolean
@@ -131,10 +212,17 @@ export const indexSignatureFilter = <T>(
   return result;
 };
 
+/**
+ * Groups all elements of the given array by the key extracted using the given indexExtractor.
+ * 
+ * @param array The array to convert.
+ * @param indexExtractor The extractor function to extract a key from each element.
+ * @returns an index signature with arrays of values grouped by keys.
+ */
 export const indexSignatureGroupBy = <T>(
   array: T[],
   indexExtractor: Function1<T, string>
-) => {
+): { [index: string]: T[] } => {
   const result: { [index: string]: T[] } = {};
   for (const value of array) {
     const index = indexExtractor(value);
@@ -145,17 +233,25 @@ export const indexSignatureGroupBy = <T>(
   return result;
 };
 
+/**
+ * Apply the given mapping function to every elements and groups all elements of the given array by the key extracted using the given indexExtractor
+ * 
+ * @param array The array to convert.
+ * @param indexExtractor The extractor function to extract a key from each element.
+ * @param mapper The mapper function.
+ * @returns an index signature with arrays of converted values grouped by keys.
+ */
 export const indexSignatureGroupByAndMap = <T, U>(
   array: T[],
   indexExtractor: Function1<T, string>,
-  converter: (element: T) => U
+  mapper: (element: T) => U
 ) => {
   const result: { [index: string]: U[] } = {};
   for (const value of array) {
     const index = indexExtractor(value);
     if (result[index] === undefined) result[index] = [];
 
-    result[index].push(converter(value));
+    result[index].push(mapper(value));
   }
   return result;
 };
