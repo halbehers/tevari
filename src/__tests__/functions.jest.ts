89e019ea6ac4af5571fe7615b65e8b379a250b39
halbehers <sebastien.halbeher@free.fr>
2024-02-07 16:17:11 +0100

    Add documentation and tests to functions

---
+++ src/__tests__/functions.jest.ts
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom";
+import Functions from "../functions";
+
+describe("#Functions.helper.identity", () => {
+  it("should return the exact given value", () => {
+    expect(Functions.helper.identity()("test")).toEqual("test");
+    expect(Functions.helper.identity()(42)).toEqual(42);
+  });
+});
+
+describe("#Functions.helper.attribute", () => {
+  it("should return the given value as the given type", () => {
+    expect(Functions.helper.attribute("name")({ name: "test" })).toEqual(
+      "test"
+    );
+  });
+});
--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,25 @@
-export const functionIdentity = <T>(value: T) => value;
+/**
+ * A function that returns the exact given value.
+ * 
+ * @param value The value parameter of the function.
+ * @returns a function that returns the exact given value.
+ */
+export const functionIdentity = () => <T>(value: T) => value;
 
-export const functionIdentityAs = <T>(value: unknown) => value as T;
+/**
+ * A function that returns the given value casted to the given type.
+ * 
+ * @param value The value parameter of the function.
+ * @returns a function that returns the given value casted to the given type.
+ */
+export const functionIdentityAs = () => <T>(value: unknown) => value as T;
 
+/**
+ * A function that extracts an attribute from the given value.
+ * 
+ * @param attributeName The name of the attribute to extract.
+ * @returns a function that extract the attrobute from the given object.
+ */
 export const functionAttribute = <T, U>(attributeName: string) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (value: T) => {
@@ -15,7 +33,12 @@ export const functionAttribute = <T, U>(attributeName: string) => {
   };
 };
 
-export const functionVoid = () => {
+/**
+ * A void function.
+ * 
+ * @returns a void function. 
+ */
+export const functionVoid = () => () => {
   // Nothing to do.
 };
 
@@ -26,19 +49,54 @@ export const FunctionHelpers = {
   void: functionVoid,
 };
 
-export type Procedure0 = () => void;
-export type Procedure1<T> = (value: T) => void;
-export type Procedure = Procedure0;
-export type Consumer<T> = Procedure1<T>;
-export type Procedure2<T, U> = (value1: T, value2: U) => void;
-export type Procedure3<T, U, V> = (value1: T, value2: U, value3: V) => void;
-export type Predicate0 = () => boolean;
-export type Predicate<T> = (value: T) => boolean;
-export type Supplier<T> = Function0<T>;
-export type Function0<T> = () => T;
+/**
+ * A void function that takes no parameter.
+ */
+export type Procedure = () => void;
+/**
+ * A void function that takes one parameter.
+ */
+export type Consumer1<T> = (value: T) => void;
+/**
+ * A void function that takes one parameter. Alias to `Consumer1`.
+ */
+export type Consumer<T> = Consumer1<T>;
+/**
+ * A void function that takes two parameters.
+ */
+export type Consumer2<T, U> = (value1: T, value2: U) => void;
+/**
+ * A void function that takes three parameters.
+ */
+export type Consumer3<T, U, V> = (value1: T, value2: U, value3: V) => void;
+/**
+ * A function that takes one parameter and returns a value.
+ */
 export type Function1<T, U> = (value: T) => U;
+/**
+ * A function that takes one parameter and returns a value. Alias to `Function1`.
+ */
+export type Function<T, U> = Function1<T, U>;
+/**
+ * A function that takes two parameters and returns a value.
+ */
 export type Function2<T, U, V> = (value1: T, value2: U) => V;
+/**
+ * A function that takes three parameters and returns a value.
+ */
 export type Function3<T, U, V, W> = (value1: T, value2: U, value3: V) => W;
+/**
+ * A function that takes no parameter and returns a value.
+ */
+export type Supplier<T> = () => T;
+/**
+ * A boolean function that takes no parameter. Alias to `Supplier<boolean>`;
+ */
+export type Predicate0 = Supplier<boolean>;
+/**
+ * A boolean function that takes one parameter.
+ */
+export type Predicate<T> = (value: T) => boolean;
 
 const Functions = {
   helper: FunctionHelpers,
